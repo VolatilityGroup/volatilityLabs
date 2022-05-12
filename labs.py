f39287fdb8d24104796e8a53a9e7ff4f3cb8b0f4
Anthony Montalbano <anthony@volatility.com>
2022-05-12 09:58:55 -0400

    Support for iframe

--- labs.py
+++ labs.py
@@ -1,52 +1,42 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 from pages import formalSpecPage, vecmPage, nftPage
-from pages import skewPage, ivSurfacePage, monteCarloPage
-from pages import garchPage
+from pages import skewPage, volatilitySurfacePage, monteCarloPage
 
 plt.style.use('ggplot')
 
 pwd = st.secrets['labsPassword']
 moralisKey = st.secrets['moralisKey']
 
-topic = ['Latent Prices Using VECM', 
-         'Formal Specification', 
-         'NFT Analytics', 
-         'Volatility Skew', 
-         'Implied Volatility Surface', 
-         'Liquidation Risk', 
-         'GARCH Model']
+st.set_page_config(layout="wide")
 
-st.title('Volatility Labs')
+topic = ['LatestPricesUsingVECM', 
+         'FormalSpecification', 
+         'NFTAnalytics', 
+         'VolatilitySkew', 
+         'ImpliedVolatilitySurface', 
+         'LiquidationRisk']
 
-pwdText = st.sidebar.text_input('Password: ', type='password')
+params = st.experimental_get_query_params()
 
-topicSelection = st.sidebar.selectbox(
-    'Select Research Topic:',
-    topic
-)
+topicSelection = params.get('topic')[0]
+queryPwd = params.get('pw')[0]
 
-if pwdText == pwd:
+if pwd == queryPwd:
+	if topicSelection == topic[0]:
+		vecmPage.render(st.secrets)
 
-    if topicSelection == topic[0]:
-        vecmPage.render(st.secrets)
+	elif topicSelection == topic[1]:
+		formalSpecPage.render()
 
-    elif topicSelection == topic[1]:
-        formalSpecPage.render()
+	elif topicSelection == topic[2]:
+		nftPage.render(moralisKey)
 
-    elif topicSelection == topic[2]:
-        nftPage.render(moralisKey)
+	elif topicSelection == topic[3]:
+		skewPage.render()
 
-    elif topicSelection == topic[3]:
-        skewPage.render(st.secrets)
+	elif topicSelection == topic[4]:
+		volatilitySurfacePage.render()
 
-    elif topicSelection == topic[4]:
-        ivSurfacePage.render(st.secrets)
-
-    elif topicSelection == topic[5]:
-        monteCarloPage.render(st.secrets)
-
-    elif topicSelection == topic[6]:
-        garchPage.render(st.secrets)
-
-
+	elif topicSelection == topic[5]:
+		monteCarloPage.render(st.secrets)
